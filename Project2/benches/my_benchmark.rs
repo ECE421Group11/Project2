38ad1b03c107a878d7a1afbff33316d1e1726249
Micheal Antifaoff <mantifao@ualberta.ca>
2020-03-01 15:28:46 -0700

    Add scale factor

--- Project2/benches/my_benchmark.rs
+++ Project2/benches/my_benchmark.rs
@@ -3,13 +3,15 @@ use Project2::avl_tree::AVLTree;
 use Project2::red_black_tree::RedBlackTree;
 use Project2::binary_tree::BinaryTree;
 
+const SCALEFACTOR: i32 = 100;
+
 fn Binary(){
     let mut vec = Vec::new();
-    vec.push(100);
-    vec.push(400);
-    vec.push(700);
-    vec.push(1000);
-    vec.push(1300);
+    vec.push(10000/SCALEFACTOR);
+    vec.push(40000/SCALEFACTOR);
+    vec.push(70000/SCALEFACTOR);
+    vec.push(100000/SCALEFACTOR);
+    vec.push(130000/SCALEFACTOR);
     for tree_size in vec{
         let mut bintree = BinaryTree::<u32>::new();
         for x in 0..tree_size{
@@ -17,17 +19,17 @@ fn Binary(){
         }
         for y in 0..(tree_size/10){
             bintree.get_node(y);
-        } 
+        }
     }
 }
 
 fn RBTree() {
     let mut vec = Vec::new();
-    vec.push(100);
-    vec.push(400);
-    vec.push(700);
-    vec.push(1000);
-    vec.push(1300);
+    vec.push(10000/SCALEFACTOR);
+    vec.push(40000/SCALEFACTOR);
+    vec.push(70000/SCALEFACTOR);
+    vec.push(100000/SCALEFACTOR);
+    vec.push(130000/SCALEFACTOR);
     for tree_size in vec{
         let mut rbtree = RedBlackTree::<u32>::new();
         for x in 0..tree_size{
@@ -35,17 +37,17 @@ fn RBTree() {
         }
         for y in 0..(tree_size/10){
             rbtree.get_node(y);
-        } 
+        }
     }
 }
 
 fn AVLTree() {
     let mut vec = Vec::new();
-    vec.push(100);
-    vec.push(400);
-    vec.push(700);
-    vec.push(1000);
-    vec.push(1300);
+    vec.push(10000/SCALEFACTOR);
+    vec.push(40000/SCALEFACTOR);
+    vec.push(70000/SCALEFACTOR);
+    vec.push(100000/SCALEFACTOR);
+    vec.push(130000/SCALEFACTOR);
     for tree_size in vec{
         let mut avltree = AVLTree::<u32>::new();
         for x in 0..tree_size{
@@ -53,14 +55,14 @@ fn AVLTree() {
         }
         for y in 0..(tree_size/10){
             avltree.get_node(y);
-        } 
+        }
     }
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("BinaryTree", |b| b.iter(|| Binary()));
     c.bench_function("RBTree", |b| b.iter(|| RBTree()));
-    c.bench_function("AVLTree", |b| b.iter(|| AVLTree())); 
+    c.bench_function("AVLTree", |b| b.iter(|| AVLTree()));
 }
 
 criterion_group!(benches, criterion_benchmark);
