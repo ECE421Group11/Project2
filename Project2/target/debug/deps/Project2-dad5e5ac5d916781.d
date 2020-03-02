e1b1d3d6bc7d56a0045afc8cb74946d8d36a9459
Andrew Williams <atwillia@ualberta.ca>
2020-03-02 16:02:55 -0700

    "Clean up"

--- Project2/target/debug/deps/Project2-dad5e5ac5d916781.d
+++
@@ -1,5 +0,0 @@
-/home/andrew/Desktop/421/Project2/Project2/target/debug/deps/Project2-dad5e5ac5d916781: src/main.rs
-
-/home/andrew/Desktop/421/Project2/Project2/target/debug/deps/Project2-dad5e5ac5d916781.d: src/main.rs
-
-src/main.rs:
--- Project2/target/debug/deps/term_painter-42480bd1c8ab5656.d
+++
@@ -1,7 +0,0 @@
-/home/andrew/Desktop/421/Project2/Project2/target/debug/deps/term_painter-42480bd1c8ab5656.rmeta: /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/term-painter-0.2.4/src/lib.rs
-
-/home/andrew/Desktop/421/Project2/Project2/target/debug/deps/libterm_painter-42480bd1c8ab5656.rlib: /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/term-painter-0.2.4/src/lib.rs
-
-/home/andrew/Desktop/421/Project2/Project2/target/debug/deps/term_painter-42480bd1c8ab5656.d: /home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/term-painter-0.2.4/src/lib.rs
-
-/home/andrew/.cargo/registry/src/github.com-1ecc6299db9ec823/term-painter-0.2.4/src/lib.rs:
